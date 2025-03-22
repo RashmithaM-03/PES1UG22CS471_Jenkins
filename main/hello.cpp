861a5a1a26d75c96d745ef42f0d805293143b57b
RashmithaM-03 <145257231+RashmithaM-03@users.noreply.github.com>
2025-03-23 00:17:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, World. Thiss is Task4\n";
+  cout << "Hello, Jenkins from PES1UG22CS471\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
